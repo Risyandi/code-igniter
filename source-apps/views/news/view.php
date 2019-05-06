933f1b63af83bfb29ee4c41844451fdf5d6ecb69
risyandi <risyandi.94@gmail.com>
2019-05-06 10:17:34 +0700

    modified view news

--- source-apps/views/news/view.php
+++ source-apps/views/news/view.php
@@ -1,4 +1,5 @@
 <div class="container shadow-sm p-5 bg-white rounde">
+<a href="<?php echo site_url('news/'); ?>"> Back to list news</a>
     <?php
     echo '<h1>'.$news_item['title'].'</h1>';
     echo $news_item['text'];
