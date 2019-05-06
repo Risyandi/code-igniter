d6209cc2fc936030ecf54211625e1a539e867928
risyandi <risyandi.94@gmail.com>
2019-05-06 10:15:48 +0700

    modified controller news

--- source-apps/controllers/News.php
+++ source-apps/controllers/News.php
@@ -9,16 +9,22 @@ class News extends CI_Controller {
         $this->load->helper('url_helper');
     }
     
+    /**
+     * method index view
+     */
     public function index(){
-        $data['news'] = $this->news_models->get_news();
+        $data['news'] = $this->news_models->getNews();
         $data['title'] = 'Listing News';
         $this->load->view('components/header', $data);
         $this->load->view('news/index', $data);
         $this->load->view('components/footer', $data);
     }
     
+    /**
+     * method read
+     */
     public function view($slug = NULL){
-        $data['news_item'] = $this->news_models->get_news($slug);
+        $data['news_item'] = $this->news_models->getNews($slug);
         if (empty($data['news_item'])) {
             show_404();
         }
@@ -29,25 +35,41 @@ class News extends CI_Controller {
         $this->load->view('components/footer', $data);
     }
 
+    /**
+     * method create
+     */
     public function create() {
         $this->load->helper('form');
         $this->load->library('form_validation');
-
+        
         $data['title'] = 'Create News';
-
+        
         $this->form_validation->set_rules('title', 'Title', 'required');
         $this->form_validation->set_rules('text', 'Text', 'required');
-
-    if ($this->form_validation->run() === FALSE) {
-        $this->load->view('components/header', $data);
-        $this->load->view('news/create');
-        $this->load->view('components/footer');
-    } else {
-        $this->news_models->set_news();
-        $this->load->view('components/header', $data);
-        $this->load->view('news/success');
-        $this->load->view('components/footer');
+        
+        if ($this->form_validation->run() === FALSE) {
+            $this->load->view('components/header', $data);
+            $this->load->view('news/create');
+            $this->load->view('components/footer');
+        } else {
+            $this->news_models->setNews();
+            $this->load->view('components/header', $data);
+            $this->load->view('news/success');
+            $this->load->view('components/footer');
+        }
     }
-}
+    
+    /**
+     * method delete
+     */
+    public function delete($id=null){
+        if(!isset($id)){
+            show_404();
+        }
+
+        if($this->news_models->deleteNews($id)){
+            redirect(site_url('/news'));
+        }
+    }
 
 }
